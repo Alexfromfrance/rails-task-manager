562b786c0af921f6342f8cb9df3fcedac443aadf
alexandre auriol <auriol.alexandre@gmail.com>
2019-05-17 17:13:34 +0200

    2exo fin

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_current_task
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    set_current_task
   end
 
   def new
@@ -17,11 +19,32 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit
+    set_current_task
+  end
+
+  def update
+    set_current_task
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    set_current_task
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
 
   def task_params
-     params.require(:task).permit(:title, :details, :completed)
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_current_task
+    if params[:id]
+      @task = Task.find(params[:id])
+    end
   end
 end
-
 
